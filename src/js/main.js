956e9b6b8ffb08d7fcdfa267b04a796ba6c6b646
chaoticsparks <ilya.andreev.it@gmail.com>
2017-06-11 04:35:02 +0300

    script for floating menu

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,31 @@
 /*
  * Custom
  */
+$( document ).ready(function() {
+   var menuDisplayPoint = 700;
+   var menuFiller = $(".menu-filler");
+   var menu = $("#menu");
+
+   $(window).scroll(function () {
+
+       var toTop = $(this).scrollTop();
+
+       if (toTop > menu.outerHeight()+20) {
+           menu.css("top", -menu.outerHeight());
+       } else if (toTop <= menu.outerHeight()+20) {
+           menu.css("position","static");
+           menuFiller.hide();
+       }
+
+       if (toTop > menuDisplayPoint) {
+           menuFiller.show();
+           menu.css("position","fixed");
+           menu.css("top","0");
+       } else if (toTop <= menuDisplayPoint) {
+
+           menu.css("top", -menu.outerHeight());
+       }
+
+
+   })
+});
